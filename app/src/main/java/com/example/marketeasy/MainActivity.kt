cb4235c72d9cb05cd0c611164e6d390ec71bc735
Yesid11 <yesidrobayo11@gmail.com>
2024-10-10 14:34:31 -0500

    Mensaje de commit

---
+++ app/src/main/java/com/example/marketeasy/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.marketeasy
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+       setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val fullText = "Bienvenido a MerkaEasy!!"
+        val spannable = SpannableString(fullText)
+        val color = ContextCompat.getColor(this, R.color.greenMain)
+        val loginButton: Button = findViewById(R.id.button)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+        spannable.setSpan(
+
+            ForegroundColorSpan(color),
+            fullText.indexOf("MerkaEasy"),
+            fullText.indexOf("MerkaEasy") + "MerkaEasy".length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        textView.text = spannable
+
+    }
+}
